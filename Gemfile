66c29d135df22b9de65e946436c08a402d2308f2
Sebastian Henao L <sebastianhenaoloaiza@gmail.com>
2021-06-28 21:44:12 -0500

    HTTP request example

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source "https://rubygems.org"
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'dotenv', '~> 2.7', '>= 2.7.6'
+gem 'httpclient', '~> 2.8', '>= 2.8.3'
+
---
+++ rest_request.rb
@@ -0,0 +1,29 @@
+require 'httpclient'
+require 'json'
+
+print 'Enter a crypto currency: '
+coin_type = gets.chomp.upcase
+
+client = HTTPClient.new
+
+begin
+    cont = client.get_content "https://api.coinbase.com/v2/exchange-rates?currency=#{coin_type}"
+    cont = JSON.parse(cont)
+
+    30.times do
+        print '-'
+    end
+
+    puts "\nValue in USD: $#{cont['data']['rates']['USD']}"
+    puts "Value in COP: #{cont['data']['rates']['COP']}$\n"
+
+    30.times do
+        print '-'
+    end
+
+rescue 
+    puts "Sorry...also went wrong. Try again"
+end
+
+
+
